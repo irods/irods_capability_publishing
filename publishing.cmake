ad1dab10b5412f4cca7fb24cd3552a3960e782c4
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2021-09-15 18:27:07 -0400

    [irods/irods#5827] CMake consistency sweep

--- publishing.cmake
+++ publishing.cmake
@@ -33,7 +33,6 @@ target_include_directories(
     ${IRODS_INCLUDE_DIRS}
     ${IRODS_EXTERNALS_FULLPATH_BOOST}/include
     ${IRODS_EXTERNALS_FULLPATH_JSON}/include
-    ${IRODS_EXTERNALS_FULLPATH_JANSSON}/include
     ${CMAKE_CURRENT_SOURCE_DIR}/include
     )
 
